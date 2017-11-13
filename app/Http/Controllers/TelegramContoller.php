0561fd33854d13f1f9091f981a9d824889017df6
Arzek <arzek2015@gmail.com>
2017-11-13 20:27:53 +0200

    api: Default command

--- app/Http/Controllers/TelegramContoller.php
+++ app/Http/Controllers/TelegramContoller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Service\TelegramService;
 use Illuminate\Http\Request;
 use Telegram;
 use Log;
@@ -10,6 +11,11 @@ class TelegramContoller extends Controller
 {
     public function webhook(Request $request)
     {
+        $chat_id = $request->input('message.chat.id');
+        $command = $request->input('message.text');
 
+        if ($command && $chat_id) {
+            TelegramService::run($command,$chat_id);
+        }
     }
 }
--- app/Service/TelegramService.php
+++ app/Service/TelegramService.php
@@ -12,6 +12,54 @@ use Telegram;
 
 class TelegramService
 {
+    /**
+     * @param string $command
+     * @param int $chat_id
+     */
+    public static function run(string $command,int $chat_id): void
+    {
+        switch ($command){
+            case '/start':{
+                self::sendMessage('Hi!',$chat_id);
+                break;
+            }
+            case '/ssl-info': {
+
+                break;
+            }
+            case '/subscribe': {
+
+                break;
+            }
+            case '/unsubscribe': {
+                break;
+            }
+            case '/help': {
+
+                break;
+            }
+            default: {
+                self::sendMessage('I do not understand you(',$chat_id);
+                break;
+            }
+        }
+    }
+
+
+    /**
+     * @param string $message
+     * @param int $chat_id
+     * @return int
+     */
+    private static function sendMessage(string $message,int $chat_id): int
+    {
+        $response = Telegram::sendMessage([
+            'chat_id' =>$chat_id,
+            'text' => $message
+        ]);
+
+        return $response->getMessageId();
+    }
 
     public static function installWebhook(): void
     {
