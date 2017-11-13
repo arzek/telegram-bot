d3cc32bf13e226fe0753415d6292bb30e3ca71f2
Arzek <arzek2015@gmail.com>
2017-11-13 19:54:58 +0200

    fix: Fix commit

---
+++ tests/Unit/Commands/TelegramInstallWebhookTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tests\Unit\Commands;
+
+use Tests\TestCase;
+
+class TelegramInstallWebhookTest extends TestCase
+{
+
+    public function testBasicTest()
+    {
+
+        $this->assertTrue(true);
+    }
+}
