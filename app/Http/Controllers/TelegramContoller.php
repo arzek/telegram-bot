9de7da730db210385117c20c32efef799c442b14
Arzek <arzek2015@gmail.com>
2017-11-13 20:15:58 +0200

    api: Create route and controller

---
+++ app/Http/Controllers/TelegramContoller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Telegram;
+use Log;
+
+class TelegramContoller extends Controller
+{
+    public function webhook(Request $request)
+    {
+
+    }
+}
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        config('app.webhook-url-min')
     ];
 }
