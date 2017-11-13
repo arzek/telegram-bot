bdde4fa2520bd7c3eb4cffeda8536530857d3254
Arzek <arzek2015@gmail.com>
2017-11-13 19:53:08 +0200

    service: Create Telegram service

---
+++ app/Service/TelegramService.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artembondarchuk
+ * Date: 13.11.17
+ * Time: 19:31
+ */
+
+namespace App\Service;
+
+use Telegram;
+
+class TelegramService
+{
+
+
+    /**
+     * @return bool
+     */
+    public static function installWebhook(): void
+    {
+        $url = config('app.webhook-url');
+        Telegram::setWebhook(['url' => $url]);
+    }
+
+    public static function uninstallWebhook(): void
+    {
+        Telegram::removeWebhook();
+    }
+}
