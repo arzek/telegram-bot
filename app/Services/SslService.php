3a80ef555c98d0e1539dde785a876ba0ab55e42f
Arzek <arzek2015@gmail.com>
2017-11-14 15:35:17 +0200

    service: Create SslService

---
+++ app/Services/SslService.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artembondarchuk
+ * Date: 12.11.17
+ * Time: 16:58
+ */
+
+namespace App\Services;
+
+use QL\QueryList;
+
+class SSLService
+{
+    private const API = 'https://www.sslshopper.com/assets/snippets/sslshopper/ajax/ajax_check_ssl.php?hostname=';
+
+    private static $document;
+
+    /**
+     * @param string $domain
+     * @return string
+     */
+    public static function check(string $domain): string {
+        self::$document =  QueryList::get(self::getLink($domain));
+
+        if(!self::isError()) {
+            return self::getMessage();
+        } else {
+            return self::getErrorMessage();
+        }
+    }
+
+    /**
+     * @param string $domain
+     * @return string
+     */
+    private static function getLink(string $domain): string {
+        return self::API.$domain;
+    }
+
+    /**
+     * @return bool|string
+     */
+    private static function isError(): bool {
+        if(count(self::$document->find('.failed')->htmls())) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private static function getMessage(): string {
+        $data = self::$document->find('.checker_messages tr td h3')->htmls();
+        $text = '';
+
+        foreach ($data as $item) {
+            if($item!='') {
+                $text.=$item."  \n ";
+            }
+        }
+
+        return $text;
+    }
+
+    /**
+     * @return string
+     */
+    private static function getErrorMessage(): string {
+        return self::$document->find('tr td h3')->text();
+    }
+
+}
